b6250a3ab52cc104896219ba00bd15a533605845
idk <problemsolver@openmailbox.org>
2019-01-23 02:00:17 -0500

    remaining lintian errors

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pin-debian-main):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pin-debian-main" command="/usr/bin/pin-debian-main"
--- debian/pin-debian-main.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pin-debian-main package
-#
-0 4	* * *	root	[ -x /usr/bin/pin-debian-main_maintenance ] && /usr/bin/pin-debian-main_maintenance
